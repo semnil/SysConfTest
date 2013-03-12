1a4d436aaa9d38fe47331171702890c2b5f599b9
Shinone Tetsuya <semnil@s4ls-MacBook-Air.local>
2013-03-12 23:22:41 +0900

    new

--- SysConfTest/STAppDelegate.m
+++ SysConfTest/STAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "STAppDelegate.h"
+#import <SystemConfiguration/SystemConfiguration.h>
 
 @implementation STAppDelegate
 
@@ -18,6 +19,47 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    SCDynamicStoreRef store;
+    SCDynamicStoreContext context;
+    CFStringRef key;
+    CFDictionaryRef val;
+    
+    NSArray *interfaces;
+    
+    memset(&context, 0, sizeof(context));
+    context.info = self;
+    store = SCDynamicStoreCreate(NULL, (CFStringRef)[[NSBundle mainBundle] bundleIdentifier], NULL, &context);
+    
+    key = SCDynamicStoreKeyCreateNetworkInterface(NULL, kSCDynamicStoreDomainState);
+    val = (CFDictionaryRef)SCDynamicStoreCopyValue(store, key);
+    interfaces = [NSArray arrayWithArray:[(NSDictionary *)val objectForKey:@"Interfaces"]];
+    CFRelease(val);
+    CFRelease(key);
+    
+    SCPreferencesRef preferences = SCPreferencesCreate(kCFAllocatorDefault, CFSTR("PRG"), NULL);
+    CFArrayRef serviceArray = SCNetworkServiceCopyAll(preferences);
+    for (int i = 0;i < CFArrayGetCount(serviceArray);i++) {
+        SCNetworkServiceRef serviceRef = (SCNetworkServiceRef)CFArrayGetValueAtIndex(serviceArray, i);
+		SCNetworkInterfaceRef interface = SCNetworkServiceGetInterface(serviceRef);
+        key = SCDynamicStoreKeyCreateNetworkInterfaceEntity(NULL, kSCDynamicStoreDomainState, SCNetworkInterfaceGetBSDName(interface), kSCEntNetIPv4);
+        val = (CFDictionaryRef)SCDynamicStoreCopyValue(store, key);
+        NSLog(@"%@, %@, %@", key, SCNetworkInterfaceGetInterfaceType(interface), [(NSDictionary *)val description]);
+        if (val != NULL)
+            CFRelease(val);
+        CFRelease(key);
+    }
+    CFRelease(serviceArray);
+    
+    /*for (NSString *interface in interfaces) {
+        key = SCDynamicStoreKeyCreateNetworkInterfaceEntity(NULL, kSCDynamicStoreDomainState, (CFStringRef)interface, kSCEntNetIPv4);
+        val = (CFDictionaryRef)SCDynamicStoreCopyValue(store, key);
+        NSLog(@"%@\n%@", key, [(NSDictionary *)val description]);
+        if (val != NULL)
+            CFRelease(val);
+        CFRelease(key);
+        
+        SCNetworkInterfaceRef interface = SCNetworkServiceGetInterface(serviceRef);
+    }*/
 }
 
 @end
